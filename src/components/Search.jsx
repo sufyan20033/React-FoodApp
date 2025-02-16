443df64f07f29c795c6e594f8001da74cc4c38f7
sufyan20033 <109736256+sufyan20033@users.noreply.github.com>
2025-02-17 03:21:29 +0500

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-const URL = "https://api.spoonacular.com/recipes/complexSearch"
-const API_KEY = "&apiKey=3edbbb67feeb4fe5b864699f4ca13afb"
+
 
 //https://api.spoonacular.com/recipes/complexSearch?query=burger&apiKey=3edbbb67feeb4fe5b864699f4ca13afb
 
